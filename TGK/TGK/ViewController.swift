30345aabaee84af857274e53f581fcf8a11e8131
Jay Park <jay@materiall.com>
2018-05-04 17:03:08 -0400

    Initial push with stubbed project

---
+++ TGK/TGK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TGK
+//
+//  Created by Jay Park on 5/4/18.
+//  Copyright © 2018 TheGivingKitchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
