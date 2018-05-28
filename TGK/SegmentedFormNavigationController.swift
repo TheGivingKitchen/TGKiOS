8e015437fc0efa2c17a84715c3382309a764f7ef
Jay Park <jay@materiall.com>
2018-05-28 11:33:56 -0400

    refactoring form nav controller

--- TGK/SegmentedFormNavigationController.swift
+++ TGK/SegmentedFormNavigationController.swift
@@ -31,6 +31,7 @@ class SegmentedFormNavigationController: UINavigationController {
         navigationBar.shadowImage = UIImage()
     }
     
+    //MARK: Paging logic
     private func setupFormPages() {
         guard let formModel = self.segmentedFormModel else {
             return
@@ -50,56 +51,79 @@ class SegmentedFormNavigationController: UINavigationController {
             self.viewControllers = [firstPage]
         }
     }
+    
+    fileprivate func nextFormPageAfter(_ formPageViewController:SegmentedFormViewController) -> SegmentedFormViewController? {
+        guard let index = self.segmentedFormViewControllers.index(where:  {$0 == formPageViewController}) else {return nil}
+        
+        let nextIndex = index + 1
+        if nextIndex >= self.segmentedFormViewControllers.count {return nil}
+        
+        return self.segmentedFormViewControllers[nextIndex]
+    }
+    
+    fileprivate func checkIfFormPageIsLastPage(_ formPageVC:SegmentedFormViewController) -> Bool {
+        guard self.segmentedFormViewControllers.count > 0 else {
+            return false
+        }
+        if formPageVC == self.segmentedFormViewControllers[self.segmentedFormViewControllers.count - 1] {
+            return true
+        }
+        return false
+    }
+    
+    //MARK: User Answer Management Logic
+    fileprivate func storeNewAnswers(_ answers:[FormQuestionAnswerModel]) {
+        //If the user has made edits to the form, replace the old answers with new ones
+        let arrayOfNewAnswerWufooIds:[String] = answers.map { (formQuestionAnswer) -> String in
+            return formQuestionAnswer.wufooFieldID
+        }
+        //Filter out answers that are about to be replaced
+        let existingQuestionsWithAnswersRemoved = self.formAnswers.filter { (formQuestionAnswer) -> Bool in
+            return !arrayOfNewAnswerWufooIds.contains(formQuestionAnswer.wufooFieldID)
+        }
+        let newAnswers = existingQuestionsWithAnswersRemoved + answers
+        self.formAnswers = newAnswers
+    }
+    
+    fileprivate func submitFormAnswers() {
+        ServiceManager.sharedInstace.submitAnswersToForm(self.segmentedFormModel.id, withAnswers: self.formAnswers) { (success, error, formFieldErrorModels) in
+            
+            if success == true {
+                self.dismiss(animated: true)
+            }
+            else {
+                if let fieldErrors = formFieldErrorModels {
+                    var firstSegmentedForWithErrors:SegmentedFormViewController?
+                    for segmentedFormVC in self.segmentedFormViewControllers {
+                        let hasErrors = segmentedFormVC.showFormFieldErrors(fieldErrors)
+                        if hasErrors && firstSegmentedForWithErrors == nil {
+                            firstSegmentedForWithErrors = segmentedFormVC
+                        }
+                    }
+                    if let firstSegmentedForWithErrors = firstSegmentedForWithErrors {
+                        self.popToViewController(firstSegmentedForWithErrors, animated: true)
+                    }
+                }
+                //other error cases where there is failure but not with fields. maybe present an alert or banner
+            }
+        }
+    }
 }
 
 //MARK: SegmentedFormViewControllerDelegate
 extension SegmentedFormNavigationController:SegmentedFormViewControllerDelegate {
     func segmentedFormViewController(_ segmentedFormViewController: SegmentedFormViewController, didAdvanceWithAnswers answers: [FormQuestionAnswerModel]) {
         
-        //If the user has made edits to the form, replace the answers
-        let arrayOfExistingAnswerWufooIds:[String] = answers.map { (formQuestionAnswer) -> String in
-            return formQuestionAnswer.wufooFieldID
-        }
-        let existingQuestionsWithAnswersRemoved = self.formAnswers.filter { (formQuestionAnswer) -> Bool in
-            return !arrayOfExistingAnswerWufooIds.contains(formQuestionAnswer.wufooFieldID)
-        }
-        let newAnswers = existingQuestionsWithAnswersRemoved + answers
-        self.formAnswers = newAnswers
+        self.storeNewAnswers(answers)
         
         //Push the next form on the stack from memory to preserve preexisting answers. Submit if you're on the last page
-        guard let index = self.segmentedFormViewControllers.index(where:  {$0 == segmentedFormViewController}) else {return}
-        
-        let nextIndex = index + 1
-        if self.segmentedFormViewControllers.count > nextIndex {
-            let nextPageVC = self.segmentedFormViewControllers[nextIndex]
-            
-            if self.segmentedFormViewControllers.count - 1 == nextIndex {
-                nextPageVC.isLastPageInForm = true
-            }
-            self.pushViewController(nextPageVC, animated: true)
+        if let nextFormPageVC = self.nextFormPageAfter(segmentedFormViewController) {
+            nextFormPageVC.isLastPageInForm = self.checkIfFormPageIsLastPage(nextFormPageVC)
+            self.pushViewController(nextFormPageVC, animated: true)
         }
-        else {
-            ServiceManager.sharedInstace.submitAnswersToForm(self.segmentedFormModel.id, withAnswers: self.formAnswers) { (success, error, formFieldErrorModels) in
-
-                if success == true {
-                    self.dismiss(animated: true)
-                }
-                else {
-                    if let fieldErrors = formFieldErrorModels {
-                        var firstSegmentedForWithErrors:SegmentedFormViewController?
-                        for segmentedFormVC in self.segmentedFormViewControllers {
-                            let hasErrors = segmentedFormVC.showFormFieldErrors(fieldErrors)
-                            if hasErrors && firstSegmentedForWithErrors == nil {
-                                firstSegmentedForWithErrors = segmentedFormVC
-                            }
-                        }
-                        if let firstSegmentedForWithErrors = firstSegmentedForWithErrors {
-                            self.popToViewController(firstSegmentedForWithErrors, animated: true)
-                        }
-                    }
-                    //other error cases where there is failure but not with fields. maybe present an alert or banner
-                }
-            }
+        //Submit if we're on the last page
+        else if segmentedFormViewController.isLastPageInForm {
+            self.submitFormAnswers()
         }
     }
     
