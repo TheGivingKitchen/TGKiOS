00b48bba6a7ed6920ce29fa2f224df0551d63adf
Jay Park <jaypark.cs@gmail.com>
2018-09-09 13:54:23 -0400

    adding first iteration of location based safetynet searching

--- TGK/SafetyNetHomeViewController.swift
+++ TGK/SafetyNetHomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class SafetyNetHomeViewController: UIViewController {
     
@@ -20,21 +21,31 @@ class SafetyNetHomeViewController: UIViewController {
     
     enum ViewState {
         case normal
-        case searching
+        case textSearching
+        case locationBased
+        case searchingWithinLocation
     }
     
     fileprivate var viewState:ViewState = .normal {
         didSet {
+            self.tableView.reloadData()
             if oldValue != viewState {
                 self.scrollToTop()
             }
+            self.configureViewControlsBasedOnState()
         }
     }
-    fileprivate var safetyNetModels:[SafetyNetResourceModel] = []
     
-    ///filtered models while searching
-    fileprivate var filteredSafetyNetModels:[SafetyNetResourceModel] = []
+    let locationManager = CLLocationManager()
+    
+    ///datasource corresponding to viewState
+    fileprivate var safetyNetModels:[SafetyNetResourceModel] = []
+    fileprivate var textSearchingSafetyNetModels:[SafetyNetResourceModel] = []
+    fileprivate var locationBasedSafetyNetModels:[SafetyNetResourceModel] = []
+    fileprivate var searchingLocationSafetyNetModels:[SafetyNetResourceModel] = []
+    
     fileprivate let searchController = UISearchController(searchResultsController: nil)
+    fileprivate var userLocationCounty:String?
     
     private let safetyNetCellReuseId = "safetyNetCellReuseId"
     private let safetyNetTooltipReuseId = "safetyNetTooltipReuseId"
@@ -43,6 +54,7 @@ class SafetyNetHomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.tableView.register(UINib(nibName: "SafetyNetInfoTableViewCell", bundle: nil), forCellReuseIdentifier: self.safetyNetCellReuseId)
         self.tableView.register(UINib(nibName: "FacebookGroupAccessTableViewCell", bundle: nil), forCellReuseIdentifier: self.safetyNetFacebookCellReuseId)
         self.tableView.register(UINib(nibName: "SafetyNetHomeTooltipCell", bundle: nil), forCellReuseIdentifier: self.safetyNetTooltipReuseId)
@@ -60,6 +72,26 @@ class SafetyNetHomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.fetchData()
+    }
+    
+    func configureViewControlsBasedOnState() {
+
+        switch self.viewState {
+        case .normal, .textSearching:
+            let locationFilterBarButton = UIBarButtonItem(image: UIImage(named: "iconLocationMarker"), style: .plain, target: self, action: #selector(changeToLocationSearchPressed(_:)))
+            self.navigationItem.rightBarButtonItem = locationFilterBarButton
+            self.navigationItem.title = ""
+        case .locationBased, .searchingWithinLocation:
+            let normalFilterBarButton = UIBarButtonItem(image: UIImage(named: "iconBulletList"), style: .plain, target: self, action: #selector(changeToNormalSearchPressed(_:)))
+            self.navigationItem.rightBarButtonItem = normalFilterBarButton
+            
+            if let userCounty = self.userLocationCounty {
+                self.navigationItem.title = "Resources in \(userCounty)"
+            }
+            else {
+                self.navigationItem.title = "Resources in your county"
+            }
+        }
     }
     
     func fetchData() {
@@ -80,6 +112,28 @@ class SafetyNetHomeViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func changeToLocationSearchPressed(_ sender: Any) {
+        self.locationManager.delegate = self
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            self.showLocationServicesDeniedAlert()
+            break
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            self.getUserLocationAndFilter()
+            break
+        }
+    }
+    
+    @IBAction func changeToNormalSearchPressed(_ sender: Any) {
+        self.searchController.searchBar.text = ""
+        self.viewState = .normal
+    }
 }
 
 extension SafetyNetHomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -90,14 +144,33 @@ extension SafetyNetHomeViewController: UITableViewDelegate, UITableViewDataSourc
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case SafetyNetHomeVCRow.tooltip.rawValue:
-            if self.viewState == .searching || AppDataStore.hasClosedSafetyNetTooltip {
+            if AppDataStore.hasClosedSafetyNetTooltip {
                 return 0
             }
-            return 1
+            switch self.viewState {
+            case .normal:
+                return 1
+            case .textSearching, .locationBased, .searchingWithinLocation:
+                return 0
+            }
         case SafetyNetHomeVCRow.facebook.rawValue:
-            return self.viewState == .searching ? 0 : 1
+            switch self.viewState {
+            case .normal:
+                return 1
+            case .textSearching, .locationBased, .searchingWithinLocation:
+                return 0
+            }
         case SafetyNetHomeVCRow.resource.rawValue:
-            return self.viewState == .normal ? self.safetyNetModels.count : self.filteredSafetyNetModels.count
+            switch self.viewState {
+            case .normal:
+                return self.safetyNetModels.count
+            case .textSearching:
+                return self.textSearchingSafetyNetModels.count
+            case .locationBased:
+                return self.locationBasedSafetyNetModels.count
+            case .searchingWithinLocation:
+                return self.searchingLocationSafetyNetModels.count
+            }
         default:
             return 0
         }
@@ -118,12 +191,23 @@ extension SafetyNetHomeViewController: UITableViewDelegate, UITableViewDataSourc
         case SafetyNetHomeVCRow.resource.rawValue:
             let cell = tableView.dequeueReusableCell(withIdentifier: self.safetyNetCellReuseId) as! SafetyNetInfoTableViewCell
             
-            let safetyNetModel = self.viewState == .normal ? self.safetyNetModels[indexPath.row] : self.filteredSafetyNetModels[indexPath.row]
-            cell.configure(withSafetyNetModel: safetyNetModel)
+            switch self.viewState {
+            case .normal:
+                cell.configure(withSafetyNetModel: self.safetyNetModels[indexPath.row])
+                break
+            case .textSearching:
+                cell.configure(withSafetyNetModel: self.textSearchingSafetyNetModels[indexPath.row])
+                break
+            case .locationBased:
+                cell.configure(withSafetyNetModel: self.locationBasedSafetyNetModels[indexPath.row])
+                break
+            case .searchingWithinLocation:
+                cell.configure(withSafetyNetModel: self.searchingLocationSafetyNetModels[indexPath.row])
+                break
+            }
+            
             cell.delegate = self
-            
             return cell
-            
         default:
             break
         }
@@ -179,16 +263,27 @@ extension SafetyNetHomeViewController: UISearchResultsUpdating {
     }
     
     func filterContentForSearchText(_ searchText:String?) {
-        ///If the user enters white space characters or is in an empty state, we're not searching
+        ///If the user cancels searching or enters white space characters or is in an empty state, we're not searching. Depending on if we're filtering by location, revert to normal or locationBased state
         guard let lowercaseTrimmedSearchText = searchText?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased(),
             lowercaseTrimmedSearchText.isEmpty == false else {
-                self.viewState = .normal
-                self.tableView.reloadData()
+                switch self.viewState {
+                case .normal:
+                    break
+                case .textSearching:
+                    self.viewState = .normal
+                    break
+                case .locationBased:
+                    break
+                case .searchingWithinLocation:
+                    self.viewState = .locationBased
+                    break
+                }
+                
                 return
         }
         
         ///If there is valid search criteria, filter the results and set us in searching state
-        self.filteredSafetyNetModels = self.safetyNetModels.filter({ (safetyNetModel) -> Bool in
+        let searchFilterBlock:(SafetyNetResourceModel) -> Bool = { (safetyNetModel) -> Bool in
             if safetyNetModel.name.lowercased().contains(lowercaseTrimmedSearchText) {
                 return true
             }
@@ -227,20 +322,110 @@ extension SafetyNetHomeViewController: UISearchResultsUpdating {
             }
             
             return false
-        })
+        }
         
-        self.viewState = .searching
-        self.tableView.reloadData()
+        self.textSearchingSafetyNetModels = self.safetyNetModels.filter(searchFilterBlock)
+        self.searchingLocationSafetyNetModels = self.locationBasedSafetyNetModels.filter(searchFilterBlock)
+        
+        switch self.viewState {
+        case .normal:
+            self.viewState = .textSearching
+            break
+        case .textSearching:
+            self.viewState = .textSearching
+            break
+        case .locationBased:
+            self.viewState = .searchingWithinLocation
+            break
+        case .searchingWithinLocation:
+            self.viewState = .searchingWithinLocation
+            break
+        }
     }
     
     func scrollToTop() {
         DispatchQueue.main.async {
-            let resourceCount = self.viewState == .normal ? self.safetyNetModels.count : self.filteredSafetyNetModels.count
+            let resourceCount = self.viewState == .normal ? self.safetyNetModels.count : self.textSearchingSafetyNetModels.count
             if resourceCount == 0 {
                 return
             }
             let indexPath = IndexPath(row: 0, section: SafetyNetHomeVCRow.resource.rawValue)
             self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
         }
+    }
+}
+
+//MARK: CLLocationManagerDelegate
+extension SafetyNetHomeViewController:CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            break
+            
+        case .restricted, .denied:
+            break
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            self.getUserLocationAndFilter()
+            break
+        }
+    }
+    
+    func getUserLocationAndFilter() {
+        self.userLocationCounty = nil
+        
+        let placesClient = GMSPlacesClient.shared()
+        placesClient.currentPlace { (placeLikelihoodList, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let placeLikelihoodList = placeLikelihoodList,
+                let place = placeLikelihoodList.likelihoods.first?.place,
+                let addressComponenets = place.addressComponents {
+                for component in addressComponenets {
+                    if component.type == "administrative_area_level_2" {
+                        self.userLocationCounty = component.name
+                        let sanitizedCountyString = component.name.lowercased().replacingOccurrences(of: "county", with: "").trimmingCharacters(in: .whitespaces)
+                        
+                        self.locationBasedSafetyNetModels = self.safetyNetModels.filter({(safetyNetModel) -> Bool in
+                            if let counties = safetyNetModel.counties {
+                                for county in counties {
+                                    if county.lowercased() == "all" {
+                                        return true
+                                    }
+                                    if county.lowercased().range(of: sanitizedCountyString) != nil {
+                                        return true
+                                    }
+                                }
+                            }
+                            return false
+                        })
+                        
+                        self.searchController.searchBar.text = ""
+                        self.viewState = .locationBased
+                    }
+                }
+            }
+        }
+    }
+    
+    func showLocationServicesDeniedAlert() {
+        let alertController = UIAlertController(title: "Enable Location Services",
+                                                message: "Location services have been turned off. Please enable them in Settings > TGK > Location to continue.",
+                                                preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Open Settings", style: .default) { (alertAction) in
+            if let appSettings = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(appSettings, options: [:], completionHandler: { (success) in
+                })
+            }
+        }
+        alertController.addAction(settingsAction)
+        
+        let cancelAction = UIAlertAction(title: "Nevermind", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true)
     }
 }
