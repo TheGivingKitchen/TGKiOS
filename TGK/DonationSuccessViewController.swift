646750caa0f1f2c87c9ac08f1654f61948d8f778
Jay Park <jaypark.cs@gmail.com>
2018-09-09 09:48:09 -0400

    making share button functional on donation success screen

--- TGK/DonationSuccessViewController.swift
+++ TGK/DonationSuccessViewController.swift
@@ -13,17 +13,17 @@ protocol DonationSuccessViewControllerDelegate:class {
 }
 
 class DonationSuccessViewController: UIViewController {
-
-    weak var delegate:DonationSuccessViewControllerDelegate?
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var taxInfoLabel: UILabel!
     
+    weak var delegate:DonationSuccessViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.welcomeLabel.font = UIFont.tgkContentSmallTitle
+        self.welcomeLabel.font = UIFont.tgkTitle
         self.welcomeLabel.textColor = UIColor.tgkOrange
         
         self.messageLabel.font = UIFont.tgkBody
@@ -37,6 +37,10 @@ class DonationSuccessViewController: UIViewController {
         
         self.taxInfoLabel.font = UIFont.tgkBody
         self.taxInfoLabel.textColor = UIColor.tgkGray
+    }
+    
+    @IBAction func sharePressed(_ sender: Any) {
+        ExternalShareManager.sharedInstance.presentShareControllerFromViewController(fromController: self, title: "I support The Giving Kitchen and helping restaurants workers in need", urlString: "https://thegivingkitchen.org/", image: UIImage(named: "tgkShareIcon"))
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
