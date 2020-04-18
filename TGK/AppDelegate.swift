84dbf9bf5a83ac5bb6ba8285a7cee5adb0d65b40
Jay Park <jaypark.cs@gmail.com>
2020-04-18 13:31:45 -0400

    all updates for version 2

--- TGK/AppDelegate.swift
+++ TGK/AppDelegate.swift
@@ -45,6 +45,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         */
         application.registerForRemoteNotifications()
         
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         return true
     }
 
